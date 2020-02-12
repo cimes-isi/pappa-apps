1180b25454dd72101187211aa14bef41d8e9f797
Mark Glines <mark@glines.org>
2020-02-12 09:33:54 -0500

    scf: Clean up Func/Input names a bit.

--- scf/halide/twoel_gen.cpp
+++ scf/halide/twoel_gen.cpp
@@ -17,16 +17,16 @@ public:
     Output<double> rv{"rv"};
 
     /* input vectors */
-    Input<Buffer<double>> expnt_unclamped{"expnt", 1};
-    Input<Buffer<double>> rnorm_unclamped{"rnorm", 1};
-    Input<Buffer<double>> x_unclamped{"x", 1};
-    Input<Buffer<double>> y_unclamped{"y", 1};
-    Input<Buffer<double>> z_unclamped{"z", 1};
+    Input<Buffer<double>> expnt_in{"expnt_in", 1};
+    Input<Buffer<double>> rnorm_in{"rnorm_in", 1};
+    Input<Buffer<double>> x_in{"x_in", 1};
+    Input<Buffer<double>> y_in{"y_in", 1};
+    Input<Buffer<double>> z_in{"z_in", 1};
 
     /* input matrices */
-    Input<Buffer<double>> fm_unclamped{"fm", 2};
-    Input<Buffer<double>> g_fock_in_unclamped{"g_fock_in", 2};
-    Input<Buffer<double>> g_dens_unclamped{"g_dens", 2};
+    Input<Buffer<double>> fm_in{"fm_in", 2};
+    Input<Buffer<double>> g_fock_in_in{"g_fock_in", 2};
+    Input<Buffer<double>> g_dens_in{"g_dens_in", 2};
 
     /* output matrix */
     Output<Buffer<double>> g_fock_out{"g_fock_out", 2};
@@ -35,18 +35,16 @@ public:
 
     void generate() {
         /* clamp all inputs, to prevent out-of-bounds errors from odd tile sizes and such */
-        Func expnt{"delo2_clamped"}, rnorm{"delo2_clamped"},
-             x{"delo2_clamped"}, y{"delo2_clamped"}, z{"delo2_clamped"},
-             fm{"delo2_clamped"}, g_fock_in{"delo2_clamped"}, g_dens{"delo2_clamped"};
+        Func expnt, rnorm, x, y, z, fm, g_fock_in, g_dens;
 
-        expnt     = BoundaryConditions::constant_exterior(expnt_unclamped    , 0);
-        rnorm     = BoundaryConditions::constant_exterior(rnorm_unclamped    , 0);
-        x         = BoundaryConditions::constant_exterior(x_unclamped        , 0);
-        y         = BoundaryConditions::constant_exterior(y_unclamped        , 0);
-        z         = BoundaryConditions::constant_exterior(z_unclamped        , 0);
-        fm        = BoundaryConditions::constant_exterior(fm_unclamped       , 0);
-        g_fock_in = BoundaryConditions::constant_exterior(g_fock_in_unclamped, 0);
-        g_dens    = BoundaryConditions::constant_exterior(g_dens_unclamped   , 0);
+        expnt     = BoundaryConditions::constant_exterior(expnt_in    , 0);
+        rnorm     = BoundaryConditions::constant_exterior(rnorm_in    , 0);
+        x         = BoundaryConditions::constant_exterior(x_in        , 0);
+        y         = BoundaryConditions::constant_exterior(y_in        , 0);
+        z         = BoundaryConditions::constant_exterior(z_in        , 0);
+        fm        = BoundaryConditions::constant_exterior(fm_in       , 0);
+        g_fock_in = BoundaryConditions::constant_exterior(g_fock_in_in, 0);
+        g_dens    = BoundaryConditions::constant_exterior(g_dens_in   , 0);
 
         Func dx{"dx"}, dy{"dy"}, dz{"dz"}, r2{"r2"}, expnt2{"expnt2"}, expnt_inv{"expnt_inv"};
 
@@ -77,7 +75,7 @@ public:
 
         Func f0t{"f0t"}, f0n{"f0n"}, f0x{"f0x"}, f0val{"f0val"};
         f0t(i,j,k,l) = fac4d(i,j,k,l) * rpq2(i,j,k,l);
-        f0n(i,j,k,l) = clamp(cast<int>((f0t(i,j,k,l) + delo2) * rdelta), fm_unclamped.dim(0).min(), fm_unclamped.dim(0).max());
+        f0n(i,j,k,l) = clamp(cast<int>((f0t(i,j,k,l) + delo2) * rdelta), fm_in.dim(0).min(), fm_in.dim(0).max());
         f0x(i,j,k,l) = delta * f0n(i,j,k,l) - f0t(i,j,k,l);
         f0val(i,j,k,l) = select(f0t(i,j,k,l) >= Expr(28.0),
              Expr(0.88622692545276) / sqrt(f0t(i,j,k,l)),
@@ -90,7 +88,7 @@ public:
         Func gg{"gg"};
         gg(i, j, k, l) = (Expr(2.00) * Expr(pow(pi, 2.50)) / denom(i,j,k,l)) * ex(i,j,k,l) * f0val(i,j,k,l) * rnorm(i) * rnorm(j) * rnorm(k) * rnorm(l);
 
-        RDom r(0,g_fock_in_unclamped.width(), 0, g_fock_in_unclamped.height());
+        RDom r(0,g_fock_in_in.width(), 0, g_fock_in_in.height());
 
         Func g_fock{"g_fock"};
         g_fock(i,j) = g_fock_in(i,j)
@@ -104,14 +102,14 @@ public:
         gg.trace_realizations();
         gg.trace_loads();
         gg.trace_stores();
-        g_dens_unclamped.trace_loads();
-        fm_unclamped.trace_loads();
-        x_unclamped.trace_loads();
-        y_unclamped.trace_loads();
-        z_unclamped.trace_loads();
-        expnt_unclamped.trace_loads();
-        rnorm_unclamped.trace_loads();
-        g_fock_in_unclamped.trace_loads();
+        g_dens_in.trace_loads();
+        fm_in.trace_loads();
+        x_in.trace_loads();
+        y_in.trace_loads();
+        z_in.trace_loads();
+        expnt_in.trace_loads();
+        rnorm_in.trace_loads();
+        g_fock_in_in.trace_loads();
         g_fock_out.trace_stores();
 #endif /* TRACING */
     }
@@ -123,18 +121,18 @@ public:
             delo2.set_estimate(0.007);
             rdelta.set_estimate(71.4285);
             /* input vectors */
-            x_unclamped.dim(0).set_estimate(0, 1024);
-            y_unclamped.dim(0).set_estimate(0, 1024);
-            z_unclamped.dim(0).set_estimate(0, 1024);
-            expnt_unclamped.dim(0).set_estimate(0, 1024);
-            rnorm_unclamped.dim(0).set_estimate(0, 1024);
+            x_in.dim(0).set_estimate(0, 1024);
+            y_in.dim(0).set_estimate(0, 1024);
+            z_in.dim(0).set_estimate(0, 1024);
+            expnt_in.dim(0).set_estimate(0, 1024);
+            rnorm_in.dim(0).set_estimate(0, 1024);
             /* input matrices */
-            g_fock_in_unclamped.dim(0).set_estimate(0, 1024);
-            g_fock_in_unclamped.dim(1).set_estimate(0, 1024);
-            g_dens_unclamped.dim(0).set_estimate(0, 1024);
-            g_dens_unclamped.dim(1).set_estimate(0, 1024);
-            fm_unclamped.dim(0).set_estimate(0, 2001);
-            fm_unclamped.dim(1).set_estimate(0, 5);
+            g_fock_in_in.dim(0).set_estimate(0, 1024);
+            g_fock_in_in.dim(1).set_estimate(0, 1024);
+            g_dens_in.dim(0).set_estimate(0, 1024);
+            g_dens_in.dim(1).set_estimate(0, 1024);
+            fm_in.dim(0).set_estimate(0, 2001);
+            fm_in.dim(1).set_estimate(0, 5);
             /* output matrix */
             g_fock_out.set_estimate(i, 0, 1024);
             g_fock_out.set_estimate(j, 0, 1024);
